91bfc5007410aaf4186bfdd255f28ae80a4330bb
Kieran Headley <kieran@hikeseo.co>
2024-08-19 16:42:38 +0100

    Fix incorrect command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('ProcessStages.php')->everyMinute();
+        $schedule->command('stages:process')->everyMinute();
     }
 
     /**
