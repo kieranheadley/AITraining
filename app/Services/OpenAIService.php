c8083bf9fc7f398e6e7d624381e2fd2f614c271a
Kieran Headley <kieran@hikeseo.co>
2024-08-23 16:14:31 +0100

    Update code

--- app/Services/OpenAIService.php
+++ app/Services/OpenAIService.php
@@ -22,11 +22,20 @@ class OpenAIService
 
     public function selectPageFromEmbeddings($keyword, $pages, $website): string
     {
-        $string = 'Primary Location: '.$website->primary_location ?? ''."\n\n";
-        $string .= 'Keyword: '.$keyword->keyword."\n\n";
+        $string = 'Primary Location: ' . ($website->primary_location ?? '') . "\n\n";
+        $string .= 'Keyword: ' . $keyword->keyword . "\n\n";
 
         foreach ($pages as $page) {
-            $string .= 'Page URL: '.$page->url."\nPage Title: ".$page->title."\nMeta Description: ".$page->meta_desc."\nHeadings: ".implode(', ', str_replace('\n', '', $page->h1_headings)).', '.implode(', ', str_replace('\n', '', $page->h2_headings))."\n\n";
+            $string .= sprintf(
+                "Page URL: %s\nPage Title: %s\nMeta Description: %s\nHeadings: %s\n\n",
+                $page->url,
+                $page->title,
+                $page->meta_desc,
+                implode(', ', array_merge(
+                    str_replace('\n', '', $page->h1_headings),
+                    str_replace('\n', '', $page->h2_headings)
+                ))
+            );
         }
 
         $client = OpenAI::client(config('services.openai.key'));
@@ -35,21 +44,11 @@ class OpenAIService
             'messages' => [
                 [
                     'role' => 'system',
-                    'content' => [
-                        [
-                            'type' => 'text',
-                            'text' => 'Role:\nYou are an expert SEO consultant tasked with identifying the most suitable page for optimising a given keyword based on SEO best practices.\n\nTask Overview:\nYou will receive key details for up to five pages on the website most relevant to the supplied keyword. This information includes the page URL, title, meta description, and headings.\nYour task is to determine whether the keyword is best optimised on an existing page or if a new page is necessary. If a primary location is provided, ensure it is prioritised in your decision.\n\nEvaluate the suitability of each page using the following criteria:\nContent Relevance: Ensure the page content closely matches the keyword.\nLocation Relevance: \n - If the keyword contains a the supplied primary location then it should be reviewed without the location being taken into consideration. (e.g. primary location = Leicester, the keyword \"Kitchen Fitting Leicester\" would be assigned to /kitchen-fitting)\n - Optimise keywords containing the primary location on national pages (e.g., /, /service, /product). Ignore location when deciding, focusing only on the keyword.\n - Optimise keywords with other locations on location-specific pages (e.g., /{location} or /{product}-{location}).\n - For multiple locations, treat them as related if geographically close and under 100,000 population. Otherwise, recommend separate pages.\nKeyword Intent: Match the intent (informational, transactional, or mixed) with the type of page. Use broad transactional keywords on the homepage; suggest blog posts for informational keywords.\n“Near Me” Keywords: Always recommend creating a new page for “near me” or similar variations.\nGeneric Pages: Never optimise keywords on generic pages like “About Us”, \"Terms & Conditions\" or “Contact Us”.\n\nResponse:\nPlease respond using the following format:\n- Page URL or “new page” if none of the supplied pages are suitable.\n- Reason for your decision: Provide a brief explanation, separated by a hyphen, like the below.\n\n[page_url|new page] - Reason\n\nExample Response:\nhttps://website.co.uk - This page is chosen because the content is highly relevant, and the keyword intent aligns with the page type.',
-                        ],
-                    ],
+                    'content' => 'Role: You are an expert SEO consultant tasked with identifying the most suitable page for optimising a given keyword based on SEO best practices. Task Overview: You will receive key details for up to five pages on the website most relevant to the supplied keyword. This information includes the page URL, title, meta description, and headings. Your task is to determine whether the keyword is best optimised on an existing page or if a new page is necessary. If a primary location is provided, ensure it is prioritised in your decision. Evaluate the suitability of each page using the following criteria: Content Relevance: Ensure the page content closely matches the keyword. Location Relevance: - If the keyword contains the supplied primary location then it should be reviewed without the location being taken into consideration. (e.g., primary location = Leicester, the keyword "Kitchen Fitting Leicester" would be assigned to /kitchen-fitting) - Optimise keywords containing the primary location on national pages (e.g., /, /service, /product). Ignore location when deciding, focusing only on the keyword. - Optimise keywords with other locations on location-specific pages (e.g., /{location} or /{product}-{location}). - For multiple locations, treat them as related if geographically close and under 100,000 population. Otherwise, recommend separate pages. Keyword Intent: Match the intent (informational, transactional, or mixed) with the type of page. Use broad transactional keywords on the homepage; suggest blog posts for informational keywords. “Near Me” Keywords: Always recommend creating a new page for “near me” or similar variations. Generic Pages: Never optimise keywords on generic pages like “About Us”, "Terms & Conditions" or “Contact Us”. Response: Please respond using the following format: - Page URL or “new page” if none of the supplied pages are suitable. - Reason for your decision: Provide a brief explanation, separated by a hyphen, like the below. [page_url|new page] - Reason Example Response: https://website.co.uk - This page is chosen because the content is highly relevant, and the keyword intent aligns with the page type.',
                 ],
                 [
                     'role' => 'user',
-                    'content' => [
-                        [
-                            'type' => 'text',
-                            'text' => $string,
-                        ],
-                    ],
+                    'content' => $string,
                 ],
             ],
             'temperature' => 1,
@@ -59,7 +58,7 @@ class OpenAIService
             'presence_penalty' => 0,
         ]);
 
-        if ($response->choices) {
+        if (isset($response->choices) && !empty($response->choices)) {
             return $response->choices[0]->message->content;
         } else {
             return 'error';
@@ -93,7 +92,7 @@ class OpenAIService
                     'content' => [
                         [
                             'type' => 'text',
-                            'text' => config('openai.keyword_selection_prompt'),
+                            'text' => 'You are an expert SEO consultant. Your task is to review the supplied keywords and select a maximum of 3 keywords for the page. Each keyword is provided with additional data in the format (Search Volume: 123, Difficulty: 10). You will also receive information about the page, including its URL, page title, meta description, and headings.\n\nWhen selecting keywords, consider the following factors in order of priority:\n\n1. Search Volume: Keywords with higher search volumes should be prioritized as they have a greater potential for attracting traffic.\n2. Difficulty: Keywords with lower difficulty scores are preferred to increase the likelihood of ranking successfully.\n3. Transactional Relevancy: While important, this factor should be considered after search volume and difficulty. Evaluate how relevant each keyword is to the transactional intent of the page, based on the provided page information.\n\nAfter reviewing the keywords, return only the 3 selected keywords as a JSON array in the format: ["keyword 1", "keyword 2", "keyword 3"].',
                         ],
                     ],
                 ],
